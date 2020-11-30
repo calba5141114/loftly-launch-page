050555ed2cdd246627569d30374c94d12b527f88
Carlos Alba <soradev4@gmail.com>
2020-11-30 23:07:09 +0000

    superficial change

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  siteTitle: 'Gatsby Starter New age', // <title>
+  siteTitle: 'Loftly Project Launch', // <title>
   manifestName: 'newage',
   manifestShortName: 'Landing', // max 12 characters
   manifestStartUrl: '/',
@@ -7,7 +7,7 @@ module.exports = {
   manifestThemeColor: '#663399',
   manifestDisplay: 'standalone',
   manifestIcon: 'src/assets/img/website-icon.png',
-  pathPrefix: `/gatsby-starter-newage/`, // This path is subpath of your hosting https://domain/portfolio
+  pathPrefix: `/`, // This path is subpath of your hosting https://domain/portfolio
   // social
   socialLinks: [
     {
--- src/components/Header.js
+++ src/components/Header.js
@@ -43,60 +43,6 @@ export default class Header extends Component {
           <a className="navbar-brand" href="#page-top">
             {config.siteTitle}
           </a>
-          <button
-            onClick={_ => this.toggleMenu(!openMenu)}
-            className={`navbar-toggler navbar-toggler-right ${
-              openMenu ? '' : 'collapsed'
-            }`}
-            type="button"
-            aria-controls="navbarResponsive"
-            aria-expanded={openMenu}
-            aria-label="Toggle navigation"
-          >
-            Menu
-            <i className="fas fa-bars"></i>
-          </button>
-
-          <div
-            className={`collapse navbar-collapse ${openMenu ? 'show' : ''}`}
-            id="navbarResponsive"
-          >
-            <ul className="navbar-nav ml-auto">
-              <li className="nav-item">
-                <Scroll
-                  onClick={_ => this.toggleMenu(!openMenu)}
-                  type="id"
-                  element="download"
-                >
-                  <a className="nav-link" href="#download">
-                    Download
-                  </a>
-                </Scroll>
-              </li>
-              <li className="nav-item">
-                <Scroll
-                  onClick={_ => this.toggleMenu(!openMenu)}
-                  type="id"
-                  element="features"
-                >
-                  <a className="nav-link" href="#features">
-                    Features
-                  </a>
-                </Scroll>
-              </li>
-              <li className="nav-item">
-                <Scroll
-                  onClick={_ => this.toggleMenu(!openMenu)}
-                  type="id"
-                  element="contact"
-                >
-                  <a className="nav-link" href="#contact">
-                    Contact
-                  </a>
-                </Scroll>
-              </li>
-            </ul>
-          </div>
         </div>
       </nav>
     );
